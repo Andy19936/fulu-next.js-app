7e8cf20b0e913bebd33e03c7f1007ad2e441a83b
LEARN Academy <learnacademy@LEARNs-Air.local>
2021-09-27 13:49:32 -0700

    base complete reusable header components completed

---
+++ components/Header.js
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import HeaderItem from './HeaderItem';
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from '@heroicons/react/outline';
+
+export const Header = () => {
+  return (
+    <header className=''>
+      <div>
+        <HeaderItem title='HOME' Icon={HomeIcon} />
+        <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+        <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+        <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+        <HeaderItem title='SEARCH' Icon={SearchIcon} />
+        <HeaderItem title='ACCOUNT' Icon={UserIcon} />
+      </div>
+      <Image
+        className='object-contain'
+        src='https://links.papareact.com/ua6'
+        width={200}
+        height={100}
+        alt='hulu'
+      />
+    </header>
+  );
+};
